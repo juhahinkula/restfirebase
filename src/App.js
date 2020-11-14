db70ff9502174e37102f3ff14dcdbde991bd08bb
juhahinkula <hinkula@gmail.com>
2020-11-14 12:53:45 +0200

    delete functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import AddTodo from './AddTodo';
 
 import './App.css';
@@ -11,7 +13,6 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [keys, setKeys] = useState([]);
 
   useEffect(() => {
     fetchItems();
@@ -27,7 +28,8 @@ function App() {
   // Add keys to the todo objects
   const addKeys = (data) => {
     const keys = Object.keys(data);
-    const valueKeys = Object.values(data).map((item, index) => Object.defineProperty(item, 'id', {value: keys[index]}));
+    const valueKeys = Object.values(data).map((item, index) => 
+      Object.defineProperty(item, 'id', {value: keys[index]}));
     setTodos(valueKeys);
   }
 
@@ -36,6 +38,15 @@ function App() {
     {
       method: 'POST',
       body: JSON.stringify(newTodo)
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+
+  const deleteTodo = (id) => {
+    fetch(`https://todolist-a1048.firebaseio.com/items/${id}.json`,
+    {
+      method: 'DELETE',
     })
     .then(response => fetchItems())
     .catch(err => console.error(err))
@@ -51,12 +62,21 @@ function App() {
         </Toolbar>
       </AppBar> 
       <AddTodo addTodo={addTodo} />     
-      <div className="ag-theme-material" style={ { height: 400, width: 600, margin: 'auto' } }>
+      <div className="ag-theme-material" style={ { height: 400, width: 700, margin: 'auto' } }>
         <AgGridReact rowData={todos}>
           <AgGridColumn sortable={true} filter={true} field='description' />
           <AgGridColumn sortable={true} filter={true} field='date' />
           <AgGridColumn sortable={true} filter={true} field='priority' />
-          <AgGridColumn hide={true} sortable={true} filter={true} field='id' />
+          <AgGridColumn 
+            headerName=''
+            field='id' 
+            width={90}
+            cellRendererFramework={ params => 
+              <IconButton onClick={() => deleteTodo(params.value)} size="small" color="secondary">
+                <DeleteIcon />
+              </IconButton>
+            }
+          />
         </AgGridReact>
       </div>
     </div>
